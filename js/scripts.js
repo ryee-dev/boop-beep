5660c0faf10bb134caecf11c0e1d028abe3f21d9
Guest User <Guest@Epicodus-8B.local>
2018-01-19 10:42:13 -0800

    adds working functions for 0's and 1's

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,63 @@
+/*
+Specifications:
+1. put user's input into an array
+2. for loop to see if the array contains any "1" or "0"
+3. if the array contains a "1", to return "Boop!", and if array contains "2", to return "Beep!"
+4. if the array contains a "3", to return "I'm sorry, Dave. I'm afraid I can't do that."
+
+*/
+var checkBeeps = function(numArray) {
+  var beepCheck = false;
+  for(var i = 0; i < numArray.length; i++) {
+    if(numArray[i] === "0") {
+      beepCheck = true;
+    }
+  }
+  return beepCheck;
+};
+
+var checkBoops = function(numArray) {
+  var boopCheck = false;
+  for(var i = 0; i < numArray.length; i++) {
+    if(numArray[i] === "1") {
+      boopCheck = true;
+    }
+  }
+  return boopCheck;
+};
+
+var isDave = function(numArray) {
+  var daveCheck = false;
+  for(var i = 0; i < numArray.length; i++) {
+    if(numArray[i] === "3") {
+      daveCheck = true;
+    }
+  }
+  return daveCheck;
+};
+
 $(document).ready(function() {
+  $("form#boopForm").submit(function(event){
     event.preventDefault();
+
+    var numInput = $("#num").val();
+    var numArray = Array.from(numInput);
+
+    var isBeep = checkBeeps(numArray);
+    var isBoop = checkBoops(numArray);
+
+    console.log(numArray);
+    console.log(checkBeeps(numArray));
+
+
+    if(isBoop === true) {
+      $("#beepBoopResult").text("Boop!");
+    } else if(isBeep === true) {
+      $("#beepBoopResult").text("Beep!");
+    } else if(isDave === true) {
+      $("#beepBoopResult").text("I'm sorry, Dave. I'm afraid I can't do that.");
+    }
+
+  });
 
 });
