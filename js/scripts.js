6eb324fd933a932293a7eb36761aaf1d76202df4
Guest User <Guest@Epicodus-8B.local>
2018-01-19 16:19:20 -0800

    fixes % 3 function

--- js/scripts.js
+++ js/scripts.js
@@ -6,45 +6,55 @@ Specifications:
 4. if the array contains a "3", to return "I'm sorry, Dave. I'm afraid I can't do that."
 
 */
-// var checkNums = function(numArray) {
-//   var numCheck = false;
+var checkNums = function(numArray) {
+  var numCheck = false;
+  for(var i = 0; i < numArray.length; i++) {
+    if(numArray[i] !== "0" && numArray[i] !== "1" && numArray[i] !== "3") {
+      numCheck = true;
+    }
+  }
+  return numCheck;
+};
+
+var checkBeeps = function(numArray) {
+  var beepCheck = false;
+  for(var i = 0; i < numArray.length; i++) {
+    if(numArray[i] === "0") {
+      beepCheck = true;
+    }
+  }
+  return beepCheck;
+};
+
+var checkBoops = function(numArray) {
+  var boopCheck = false;
+  for(var i = 0; i < numArray.length; i++) {
+    if(numArray[i] === "1") {
+      boopCheck = true;
+    }
+  }
+  return boopCheck;
+};
+
+// var checkDave = function(numArray) {
+//   var daveCheck = false;
 //   for(var i = 0; i < numArray.length; i++) {
-//     if(numArray[i] !== "0" && numArray[i] !== "1" && numArray[i] !== "3") {
-//       numCheck = true;
+//     if(numArray[i] === "3") {
+//       daveCheck = true;
 //     }
 //   }
-//   return numCheck;
+//   return daveCheck;
 // };
-//
-// var checkBeeps = function(numArray) {
-//   var beepCheck = false;
-//   for(var i = 0; i < numArray.length; i++) {
-//     if(numArray[i] === "0") {
-//       beepCheck = true;
-//     }
-//   }
-//   return beepCheck;
-// };
-//
-// var checkBoops = function(numArray) {
-//   var boopCheck = false;
-//   for(var i = 0; i < numArray.length; i++) {
-//     if(numArray[i] === "1") {
-//       boopCheck = true;
-//     }
-//   }
-//   return boopCheck;
-// };
-//
-// var checkMod = function(numArray) {
-//   var modCheck = false;
-//   for(var i = 0; i < numArray.length; i++) {
-//     if(numArray[i] % "3" === 0) {
-//       modCheck = true;
-//     }
-//   }
-//   return modCheck;
-// };
+
+var checkMod = function(numInput) {
+  var modCheck = false;
+    if(numInput % 3 === 0) {
+      modCheck = true;
+    }
+  return modCheck;
+};
+
+
 
 $(document).ready(function() {
   $("form#boopForm").submit(function(event){
@@ -53,57 +63,50 @@ $(document).ready(function() {
     var nameInput = $("#name").val();
     var numInput = $("#num").val();
     var numArray = Array.from(numInput);
-    var outputArray = [];
 
-    var replaceArray = function() {
-      for (var i = 0; i < numArray.length; i++) {
-        if(numArray[i] !== "0" && numArray[i] !== "1" && (numArray[i] % 3) !== "0") {
-          outputArray.push(numArray[i]);
-        } else if (numArray[i] === "0") {
-          outputArray.push(" Beep! ");
-        } else if (numArray[i] === "1") {
-          outputArray.push(" Boop! ");
-        } else if (numArray[i] % 3 === "0") {
-          outputArray.push(" I'm sorry, " + nameInput + ". I'm afraid I can't do that.");
-        }
-      }
-    };
+    var notOne = checkNums(numArray);
+    var isBeep = checkBeeps(numArray);
+    var isBoop = checkBoops(numArray);
+    // var isDave = checkDave(numArray);
+    var divisible = checkMod(numInput);
 
-    replaceArray(numArray);
+    console.log(numArray);
+    console.log(divisible);
 
-    console.log(replaceArray(numArray));
+    if(nameInput === '') {
+      $("#beepBoopResult").text("Oops, you forgot to enter your name.");
+      $("#SURPRISE").fadeIn(800);
+      $("#boopForm")[0].reset();
+    }
 
-    // var notOne = checkNums(numArray);
-    // var isBeep = checkBeeps(numArray);
-    // var isBoop = checkBoops(numArray);
-    // var divisible = checkMod(numArray);
+    if(divisible === true) {
+      $("#beepBoopResult").text("I'm sorry, " + nameInput + ". I'm afraid I can't do that.");
+      $("#SURPRISE").fadeIn(800);
+      $("#boopForm")[0].reset();
 
-
-
-
-    // if(nameInput === '') {
-    //   $("#beepBoopResult").text("Oops, you forgot to enter your name.");
+    // } else if(isDave === true) {
+    //   $("#beepBoopResult").text("I'm sorry, Dave. I'm afraid I can't do that.");
     //   $("#SURPRISE").fadeIn(800);
     //   $("#boopForm")[0].reset();
-    // } else if(divisible === true) {
-    //   $("#beepBoopResult").text("I'm sorry, " + nameInput + ". I'm afraid I can't do that.");
-    //   $("#SURPRISE").fadeIn(800);
-    //   $("#boopForm")[0].reset();
-    // } else if (isBoop === true) {
-    //   $("#beepBoopResult").text("Boop!");
-    //   $("#SURPRISE").fadeIn(800);
-    //   $("#boopForm")[0].reset();
-    // } else if(isBeep === true) {
-    //   $("#beepBoopResult").text("Beep!");
-    //   $("#SURPRISE").fadeIn(800);
-    //   $("#boopForm")[0].reset();
-    // } else if (notOne === true) {
-    //   $("#beepBoopResult").text(numInput);
-    //   $("#SURPRISE").fadeIn(800);
-    //   $("#boopForm")[0].reset();
-    // } else {
-    //   $("#beepBoopResult").text("Oops, you forgot to enter a number.");
-    // }
+
+    } else if (isBoop === true) {
+      $("#beepBoopResult").text("Boop!");
+      $("#SURPRISE").fadeIn(800);
+      $("#boopForm")[0].reset();
+
+    } else if(isBeep === true) {
+      $("#beepBoopResult").text("Beep!");
+      $("#SURPRISE").fadeIn(800);
+      $("#boopForm")[0].reset();
+
+    } else if (notOne === true) {
+      $("#beepBoopResult").text(numInput);
+      $("#SURPRISE").fadeIn(800);
+      $("#boopForm")[0].reset();
+    } else {
+      $("#beepBoopResult").text("Oops, you forgot to enter a number.");
+    }
 
   });
+
 });
