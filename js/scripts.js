4468100ff0a3386b97aaf467d3fc20140ec75bf4
roger yee <ryee.epicodus@gmail.com>
2018-01-29 18:55:47 -0800

    properly formats business-logic and front-end logic

--- js/scripts.js
+++ js/scripts.js
@@ -4,18 +4,7 @@ Specifications:
 2. for loop to see if the array contains any "1" or "0"
 3. if the array contains a "1", to return "Boop!", and if array contains "2", to return "Beep!"
 4. if the array contains a "3", to return "I'm sorry, Dave. I'm afraid I can't do that."
-
 */
-
-// var boopCheck = function(numArray) {
-//   var checkBoop = false;
-//   for(var i = 0; i < numArray.length; i++) {
-//     if(numArray[i] === "1" && numArray[i+1] === "0") {
-//       checkBoop = true;
-//     }
-//   }
-//   return checkBoop;
-// };
 
 var checkMod = function(numInput) {
   var modCheck = false;
@@ -25,48 +14,51 @@ var checkMod = function(numInput) {
   return modCheck;
 };
 
-$(document).ready(function() {
-  $("form#boopForm").submit(function(event){
-
-    var nameInput = $("#name").val();
-    var numInput = $("#num").val();
-    var numArray = Array.from(numInput);
-    // var isBoop = boopCheck(numArray);
-    var divisible = checkMod(numInput);
-
-    var toOutput = function() {
-      var outputArray = [];
-      for(var i = 0; i < numArray.length; i++) {
-        if (numArray[i] === "1" && numArray[i+1] === "0") {
-          outputArray.push("Boop!");
-        } else if (numArray[i] === "0" && numArray[i-1] === "1") {
-          outputArray.push();
-        } else if (numArray[i] === "1") {
-          outputArray.push("Boop!");
-        } else if (numArray[i] === "0") {
-          outputArray.push("Beep!");
-        } else {
-          outputArray.push(numArray[i]);
-        }
-
+var toOutput = function(numArray) {
+  var outputArray = [];
+  for(var i = 0; i < numArray.length; i++) {
+    if (numArray[i] === "1" && numArray[i+1] === "0") {
+      outputArray.push("Boop!");
+    } else if (numArray[i] === "0" && numArray[i-1] === "1") {
+      outputArray.push();
+    } else if (numArray[i] === "1") {
+      outputArray.push("Boop!");
+    } else if (numArray[i] === "0") {
+      outputArray.push("Beep!");
+    } else {
+      outputArray.push(numArray[i]);
     }
-      return outputArray;
-    }
+  }
+  return outputArray;
+};
 
-    if (divisible === true && nameInput === "") {
-      $("#beepBoopResult").text("Oops, you forgot to enter your name.");
-      $("#SURPRISE").fadeIn(800);
-      $("#boopForm")[0].reset();
+var inputType = function(divisible, nameInput, numArray) {
+  if (divisible === true && nameInput === "") {
+    $("#beepBoopResult").text("Oops, you forgot to enter your name.");
+    $("#SURPRISE").fadeIn(800);
     } else if (divisible === true && nameInput !== "") {
       $("#beepBoopResult").text("I'm sorry, " + nameInput + ". I'm afraid I can't do that.");
       $("#SURPRISE").fadeIn(800);
       $("#boopForm")[0].reset();
     } else {
-      $("#beepBoopResult").text(toOutput().join("-"));
+      $("#beepBoopResult").text(toOutput(numArray).join("-"));
       $("#SURPRISE").fadeIn(800);
       $("#boopForm")[0].reset();
-      console.log(toOutput());
     }
+  };
+
+
+$(document).ready(function() {
+  $("form#boopForm").submit(function(event){
     event.preventDefault();
+
+    var nameInput = $("#name").val();
+    var numInput = $("#num").val();
+    var numArray = Array.from(numInput);
+    var divisible = checkMod(numInput);
+
+    $("#SURPRISE").show();
+    $("#beepBoopResult").text(inputType(divisible, nameInput, numArray));
+
   });
 });
